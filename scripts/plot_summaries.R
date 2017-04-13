5519f6f04e82110eb5f2dcc1a602f6084383e579
Scott Farley <sfarley2@wisc.edu>
2017-04-13 10:38:53 -0500

    Add data files and scripts for generating cumulative record diagrams

---
+++ scripts/plot_summaries.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+library(reshape2)
+library(randomcoloR)
+
+setwd("/users/scottsfarley/documents/qsr_manuscript")
+
+nds <- read.csv("data/neotoma_datasets_by_type.csv")
+ndsMelt <- melt(nds, id.vars=c("X"))
+#make the plot
+ggplot(ndsMelt, aes(x=as.factor(X), y=value,group=variable,fill=variable)) + 
+  scale_x_discrete() +
+  geom_area(alpha = 1) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(values=randomColor(length(unique(ndsMelt$variable)))) +
+  ggtitle("Neotoma Datasets") +
+  ylab("Cumulative Number of Datasets") +
+  xlab("Year")
+
+
+nocc <- read.csv("data/neotoma_occurrences_by_type.csv")
+noMelt <- melt(nocc, id.vars=c("X"))
+#make the plot
+ggplot(noMelt, aes(x=as.factor(X), y=value,group=variable,fill=variable)) + 
+  scale_x_discrete() +
+  geom_area(alpha = 1) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(values=randomColor(length(unique(ndsMelt$variable)))) +
+  ggtitle("Neotoma Occurrences") +
+  ylab("Cumulative Number of Records") +
+  xlab("Year")
---
+++ scripts/summarize_records.R
@@ -0,0 +1,76 @@
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(randomcoloR)
+
+setwd("/users/scottsfarley/documents/qsr_manuscript")
+
+dat <- read.csv("data/neotoma_scrape_4-2017.csv")
+
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+
+dat$Type <- as.factor(dat$Type)
+dat$year <- substrRight(as.character(dat$Upload), 4)
+dat$year <- as.factor(dat$year)
+
+dat_split <- split(dat, dat$Type)
+
+doProcess <- function(dt, datatype){
+  summary <- ddply(dt, .(year), summarise, numRecords = sum(Occurrences), numDS = length(Occurrences))
+  summary$cumsum <- cumsum(summary$numRecords)
+  summary$numDS <- cumsum(summary$numDS)
+  return(summary)
+}
+
+summaries <- list()
+
+for (i in dat_split){
+  datatype <- as.character(i$Type)[1]
+  print(datatype)
+  summary <- doProcess(i, datatype)
+  summaries[[datatype]]<- summary
+  print(counter)
+}
+# Summarize occurrences
+summaryMelt <- melt(summaries, id.vars=c('year'))
+summaryMelt$DatasetType <- summaryMelt$L1 #CHANGE COL NAME
+# dsMelt$DatasetType <- dsMelt$L1 #CHANGE COL NAMES
+summaryMelt$cumsum <- summaryMelt$value
+summaryMelt <- summaryMelt[summaryMelt$variable == 'cumsum', ]
+summaryMelt$year = as.numeric(as.character(summaryMelt$year))
+summaryMelt <- summaryMelt[summaryMelt$year > 2000,]
+summaryMelt$year = as.factor(summaryMelt$year)
+summaryMelt <- na.omit(summaryMelt)
+
+summaryMelt$L1 <- NULL
+summaryMelt$value <- NULL
+summaryMelt$variable <- NULL
+
+occurrences_summary <- summaryMelt[order(summaryMelt$year, summaryMelt$DatasetType),]
+
+write.csv(occurrences_summary, "data/occurrences_summary_raw.csv")
+
+
+
+
+dsMelt <- melt(summaries, id.vars=c('year'))
+dsMelt$DatasetType <- dsMelt$L1 #CHANGE COL NAMES
+dsMelt$cumsum <- dsMelt$value
+dsMelt <- dsMelt[dsMelt$variable == 'numDS', ]
+dsMelt$year = as.numeric(as.character(dsMelt$year))
+dsMelt <- dsMelt[dsMelt$year > 2000,]
+dsMelt$L1 <- NULL
+dsMelt$value <- NULL
+dsMelt$variable <- NULL
+
+ds_summary <- dsMelt[order(dsMelt$year, dsMelt$DatasetType),]
+
+write.csv(ds_summary, "data/datasets_summary_raw.csv")
+
+
+
